79d2469a9a098bdb273c9a0dede3158609ab1fac
jzhugithub <mfx106825372@163.com>
2017-07-17 22:07:48 +0800

    update

--- Res_model/res_data.py
+++ Res_model/res_data.py
@@ -5,7 +5,7 @@ from xml.etree import ElementTree
 import os, sys
 import random
 from sklearn.model_selection import train_test_split
-import cPickle as pickle
+import pickle
 import numpy as np
 import skimage.io
 import skimage.transform
@@ -236,8 +236,15 @@ def generate_label_file(nplabels, file_path):
     np.save(os.path.abspath(file_path), nplabels)
 
 
+def create_folder(folder_path_list):
+    for folder_path in folder_path_list:
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+
 def generate_path_label():
     print('generate_path_label()')
+    create_folder(['../data', '../data/id_image', './result', './result/test_features'])
     # import train and valid path
     id_path = get_id_path_dict('../data/id_path.pkl')
     # split train and valid path
@@ -249,7 +256,7 @@ def generate_path_label():
     print('valid id: ' + str(len(X_valid)))
     # generate train file
     # if your internal storage is more than 16g, recommend use load mode!!!
-    # generate_id_image_file(X_train, '../data/id_image/id_image_train_%s_%s_%s.pkl', id_num_in_part=3000) # load mode
+    generate_id_image_file(X_train, '../data/id_image/id_image_train_%s_%s_%s.pkl', id_num_in_part=3000) # load mode
     with open('../data/id_path_train.pkl', "wb") as f:
         pickle.dump(X_train, f)
     # generate train_200 file
